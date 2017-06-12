24e0268de72675ded195d5a07481639eaf9c24dc
Vince Roti <vince.roti@gmail.com>
2017-06-11 19:47:50 -0700

    move token var up

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const bodyParser = require('body-parser')
 const request = require('request')
 const app = express()
 
+const token = process.env.FB_PAGE_ACCESS_TOKEN
+
 app.set('port', (process.env.PORT || 5000))
 
 // Process application/x-www-form-urlencoded
@@ -39,8 +41,6 @@ app.post('/webhook/', function (req, res) {
     }
     res.sendStatus(200)
 })
-
-const token = process.env.FB_PAGE_ACCESS_TOKEN
 
 // Spin up the server
 app.listen(app.get('port'), function() {
