bdde3fc9680a5fcb40645c7824c070c4b70a31fa
Vince Roti <vince.roti@gmail.com>
2017-06-11 19:44:33 -0700

    add env

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 'use strict'
 
+const env = require('node-env-file');
 const express = require('express')
 const bodyParser = require('body-parser')
 const request = require('request')
@@ -25,6 +26,21 @@ app.get('/webhook/', function (req, res) {
   }
   res.send('Error, wrong token')
 })
+
+app.post('/webhook/', function (req, res) {
+    let messaging_events = req.body.entry[0].messaging
+    for (let i = 0; i < messaging_events.length; i++) {
+      let event = req.body.entry[0].messaging[i]
+      let sender = event.sender.id
+      if (event.message && event.message.text) {
+        let text = event.message.text
+        sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+      }
+    }
+    res.sendStatus(200)
+})
+
+const token = process.env.FB_PAGE_ACCESS_TOKEN
 
 // Spin up the server
 app.listen(app.get('port'), function() {
